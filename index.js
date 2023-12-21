6027d96b0013cd37fc4d54d327648093c93cf6c1
Nelli Sviili <nelli.sviili@gmail.com>
2023-12-21 13:19:35 +0100

    teht 3.13 testailua

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 // jatketaan työskentelyä puhelinluettelon backendin kanssa
 // tässä vaiheessa koodataan ohjelma hakemaan puh.numerot tietokannasta
 // step2 puh.numerot myös tallennetaan tietokantaan 
+// luotu oma moduulinsa ./models/person 
+// tallennettu ympäristömuuttuja MONGODB_URI .env tiedostoon ja gitignorattu se
 
-// const express = require('express')
-// const morgan = require('morgan')
-// const app = express()
+require('dotenv').config() // ympäristömuuttuja käyttöön ennen ./models/person importtia
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 const mongoose = require('mongoose')
+
+const Person = require('./models/person') // ottaa modulin käyttöön
 
 // app.use(express.json((req, res, data) => {
 //   req.rawBody = data.toString();
@@ -18,42 +23,26 @@ const mongoose = require('mongoose')
 
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const password = process.argv[2]
+const url = process.env.MONGODB_URI // url haetaan ympäristömuuttujasta
+console.log('connecting to', url)
 
-const url =
-  `mongodb+srv://nellisviili:2nGbwsBjdgMKvjGH@cluster0.awpefb8.mongodb.net/` // TODO: ${password}
+mongoose.connect(url) // yhdistetään haettuun urliin
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    phonenumber: String,
+const PORT = process.env.PORT // portin aktivointi .env kautta
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
-  
-const Person = mongoose.model('Person', personSchema)
 
 let persons = [  
-  {    
-    id: 1, 
-    name: "Arto Hellas",
-    number: "040-123456",
-  }, 
-  {    
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  }, 
-  {    
-    id: 3,
-    name: "Dan Abranov",
-    number: "12-43-234345",
-  }, 
-  {    
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122",
-  }
 ]
 
 const generateId = () => {
@@ -85,18 +74,19 @@ app.post('/api/persons', (request, response) => {
     id: generateId(),
     name: body.name,
     number: body.number,
-    
   }
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 // etusivu
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
+
+app.get('/favicon.ico', (req, res) => res.status(204)) // poistaa favicon.ico 404 virheilmon
 
 // tallennetut henkilöt MondoDB:stä JSON 
 app.get('/api/persons', (request, response) => {
@@ -117,24 +107,16 @@ app.get('/info', (request, response) => {
 
 // tallennettu henkilö indeksissä x
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {    
-    response.json(person)  
-  } else {    
-    response.status(404).end()  
-  }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
 
+/* ei vielä tarpeen
 // henkilön poisto
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   
   response.status(204).end()
-})
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+}) */
